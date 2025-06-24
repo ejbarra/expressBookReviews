ccc4fec3c79b26b6396a8cc6c31ce3428d986717
ejbarra <erikjbarra@gmail.com>
2025-06-24 10:56:55 -0400

    t10

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -41,9 +41,47 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.session.authorization.username;
+    
+    if (!review) {
+        return res.status(400).json({message: "Review is required as query parameter"});
+    }
+    
+    if (!books[isbn]) {
+        return res.status(404).json({message: "Book not found"});
+    }
+
+    books[isbn].reviews[username] = review;
+    
+    return res.status(200).json({
+        message: "Review successfully added/updated",
+        reviews: books[isbn].reviews
+    });
+
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+    
+    if (!books[isbn]) {
+        return res.status(404).json({message: "Book not found"});
+    }
+    
+    if (!books[isbn].reviews[username]) {
+        return res.status(404).json({message: "Review not found for this user"});
+    }
+    
+    delete books[isbn].reviews[username];
+    
+    return res.status(200).json({
+        message: "Review successfully deleted",
+        reviews: books[isbn].reviews
+    });
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
